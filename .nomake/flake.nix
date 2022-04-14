5c15160512a16105b2419b1b0d847dea824e92ca
Akira Komamura <akira.komamura@gmail.com>
2022-04-14 15:54:57 +0900

    chore: Add nomake

---
+++ .nomake/flake.nix
@@ -0,0 +1,13 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    org-bb = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-blockers-block";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
